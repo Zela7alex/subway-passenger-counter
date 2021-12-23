321c66c8823b1907b268b842e47d465d4a558ebc
Alexandra Nunez <93490778+Zela7alex@users.noreply.github.com>
2021-12-23 16:08:30 -0500

    reorganized repository location

---
+++ script.js
@@ -0,0 +1,33 @@
+ // Defining the count element 
+ // let countEl = document.getElementById("count-el");
+
+//Checking to see if it's right on the console
+console.log(countEl)
+
+//Must start it at zero
+let count = 0;
+console.log(count);
+
+
+// Creating the function to make the "count" increment by 1 with each click.
+
+function increment() {
+    count += 1 ;
+    countEl.textContent = count;
+    console.log(count);
+}
+
+let saveEl = document.getElementById("save")
+
+
+function save() {
+    let countSave = count + " - ";
+    saveEl.textContent += countSave;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+
+
+
+
